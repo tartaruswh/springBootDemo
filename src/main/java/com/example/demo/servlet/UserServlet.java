fc6288c9d4b50866fc514d84c27c406bcc027e9f
tartaruswh <15840097953@163.com>
2023-02-22 23:21:20 +0800

    servlet进行请求实现

---
+++ src/main/java/com/example/demo/servlet/UserServlet.java
@@ -0,0 +1,26 @@
+package com.example.demo.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "servlet请求测试",urlPatterns = "/api/v1/test/customs")
+public class UserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("thi is my custom servlet");
+        printWriter.flush();
+        printWriter.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+}
